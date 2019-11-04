9866a3a579c0e872b42781f781b048537b2f96b3
user <user@DESKTOP-JLT3TAT>
2019-11-05 00:38:43 +0900

    test

--- gradPortfolio/src/main/java/com/example/demo/GradPortfolioApplication.java
+++ gradPortfolio/src/main/java/com/example/demo/GradPortfolioApplication.java
@@ -2,19 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.common.*")
 @SpringBootApplication
-public class GradPortfolioApplication extends SpringBootServletInitializer{
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(GradPortfolioApplication.class);
-	}
-	
+public class GradPortfolioApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GradPortfolioApplication.class, args);

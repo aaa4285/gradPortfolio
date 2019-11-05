a355df97c95b71015c55a1539898bcb0aa56bb03
user <user@DESKTOP-JLT3TAT>
2019-11-05 21:47:49 +0900

    update

--- gradPortfolio/src/main/java/com/common/login/controller/LoginController.java
+++ gradPortfolio/src/main/java/com/common/login/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.common.constantes.BaseConstantes;
+
 @Controller
 public class LoginController {
 	

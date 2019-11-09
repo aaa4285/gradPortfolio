a9c9baebdb1e2b839954c7ff3796bb6146dc5441
JMG <aaa4285@naver.com>
2019-11-10 02:01:24 +0900

    aaa

--- gradPortfolio/src/main/java/com/common/login/google/GoogleOAuth2ClientAuthenticationProcessingFilter.java
+++ gradPortfolio/src/main/java/com/common/login/google/GoogleOAuth2ClientAuthenticationProcessingFilter.java
@@ -27,36 +27,5 @@ public class GoogleOAuth2ClientAuthenticationProcessingFilter extends OAuth2Clie
         super("/login/google");
         this.socialService = socialService;
     }
-    
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-    		throws AuthenticationException, IOException, ServletException {
-    	return super.attemptAuthentication(request, response);
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        // super.successfulAuthentication(request, response, chain, authResult);
-        // Nearly a no-op, but if there is a ClientTokenServices then the token will now be stored
-
-        final OAuth2AccessToken accessToken = restTemplate.getAccessToken();
-        final OAuth2Authentication auth = (OAuth2Authentication) authResult;
-        final Object details = auth.getUserAuthentication().getDetails();
-
-        final GoogleUserDetails userDetails = mapper.convertValue(details, GoogleUserDetails.class);
-        userDetails.setAccessToken(accessToken);
-        final UserConnection userConnection = UserConnection.valueOf(userDetails);
-        
-        final UsernamePasswordAuthenticationToken authenticationToken = socialService.doAuthentication(userConnection);
-        super.successfulAuthentication(request, response, chain, authenticationToken);
-
-    }
-    
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-    		AuthenticationException failed) throws IOException, ServletException {
-    	// TODO Auto-generated method stub
-    	super.unsuccessfulAuthentication(request, response, failed);
-    }
 
 }

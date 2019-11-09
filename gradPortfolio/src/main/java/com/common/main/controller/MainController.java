206baf914e51385b42732ec4697d785632865706
Nahyun <Nahyun@Nahyun-PC>
2019-11-09 22:22:08 +0900

    차트반영

--- gradPortfolio/src/main/java/com/common/main/controller/MainController.java
+++ gradPortfolio/src/main/java/com/common/main/controller/MainController.java
@@ -1,12 +1,9 @@
 package com.common.main.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,9 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.common.open.api.service.OpenApiService;
-import com.common.session.MenuVO;
-import com.common.session.SessionVO;
-import com.common.session.UserVO;
+import com.common.util.CommonUtils;
 
 
 @Controller
@@ -31,63 +26,7 @@ public class MainController {
 		
 		String view = request.getServletPath().substring(1,request.getServletPath().lastIndexOf("."));
 		
-		HttpSession session = request.getSession(true);
-		
-		// session check
-		if (session.getAttribute("sessionVO") == null) {
-			/*
-			model.addAttribute("errMsg","로그인을 해주세요.");
-			model.addAttribute("returnPage","/");
-			model.addAttribute("returnParam","");
-			model.addAttribute("returnType","");
-			return "error";
-			*/
-			// TODO UserVO
-	        UserVO userVO = new UserVO();
-			//userVO.setId(userInfo.get("email").toString());
-	        userVO.setId("testID"); // TODO
-			userVO.setName("testNm"); // TODO
-			
-	        // TODO 메뉴 관리 어캐할거냐?
-			List<MenuVO> menuList = new ArrayList();
-			MenuVO menuVO = new MenuVO();
-			menuVO.setIdx(1);
-			menuVO.setTitle("Home");
-			menuVO.setUrl("/main/index.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(2);
-			menuVO.setTitle("유기동물 조회");
-			menuVO.setUrl("/main/index2.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(3);
-			menuVO.setTitle("보호중");
-			menuVO.setUrl("/main/index3.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(4);
-			menuVO.setTitle("찾아요");
-			menuVO.setUrl("/main/index4.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(5);
-			menuVO.setTitle("자유게시판");
-			menuVO.setUrl("/board/list");
-			menuList.add(menuVO);
-			
-			SessionVO sessionVO = new SessionVO();
-			sessionVO.setUserVO(userVO);
-			sessionVO.setMenuIdx(1);
-			sessionVO.setMenuList(menuList);
-			
-			session.setAttribute("sessionVO", sessionVO);
-		}
-		
-		// session menu save
-		SessionVO sessionVO = (SessionVO)session.getAttribute("sessionVO");
-		sessionVO.setMenuIdx(1);
-		session.setAttribute("sessionVO", sessionVO);
+		CommonUtils.setSession(request, 1);
 		
 		// service
 		Map<String, Object> searchJSON = new HashMap<String, Object>();
@@ -100,63 +39,7 @@ public class MainController {
 		
 		String view = request.getServletPath().substring(1,request.getServletPath().lastIndexOf("."));
 		
-		HttpSession session = request.getSession(true);
-		
-		// session check
-		if (session.getAttribute("sessionVO") == null) {
-			/*
-			model.addAttribute("errMsg","로그인을 해주세요.");
-			model.addAttribute("returnPage","/");
-			model.addAttribute("returnParam","");
-			model.addAttribute("returnType","");
-			return "error";
-			*/
-			// TODO UserVO
-	        UserVO userVO = new UserVO();
-			//userVO.setId(userInfo.get("email").toString());
-	        userVO.setId("testID"); // TODO
-			userVO.setName("testNm"); // TODO
-			
-	        // TODO 메뉴 관리 어캐할거냐?
-			List<MenuVO> menuList = new ArrayList();
-			MenuVO menuVO = new MenuVO();
-			menuVO.setIdx(1);
-			menuVO.setTitle("Home");
-			menuVO.setUrl("/main/index.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(2);
-			menuVO.setTitle("유기동물 조회");
-			menuVO.setUrl("/main/index2.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(3);
-			menuVO.setTitle("보호중");
-			menuVO.setUrl("/main/index3.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(4);
-			menuVO.setTitle("찾아요");
-			menuVO.setUrl("/main/index4.do");
-			menuList.add(menuVO);
-			menuVO = new MenuVO();
-			menuVO.setIdx(5);
-			menuVO.setTitle("자유게시판");
-			menuVO.setUrl("/main/index5.do");
-			menuList.add(menuVO);
-			
-			SessionVO sessionVO = new SessionVO();
-			sessionVO.setUserVO(userVO);
-			sessionVO.setMenuIdx(1);
-			sessionVO.setMenuList(menuList);
-			
-			session.setAttribute("sessionVO", sessionVO);
-		}
-		
-		// session menu save
-		SessionVO sessionVO = (SessionVO)session.getAttribute("sessionVO");
-		sessionVO.setMenuIdx(2);
-		session.setAttribute("sessionVO", sessionVO);
+		CommonUtils.setSession(request, 2);
 		
 		// service
 		Map<String, Object> searchJSON = new HashMap<String, Object>();
--- gradPortfolio/src/main/webapp/common/js/common_utils.js
+++ gradPortfolio/src/main/webapp/common/js/common_utils.js
@@ -53,3 +53,28 @@ function yyyyMMddFormat(str,c){
 	var d = str.substring(6,8);
 	return y+c+m+c+d;
 }
+
+function getMapCodeInfo(cd){
+	//var mapper = [{"name":"강원도","upr_cd":"6420000","map_id":"42"},{"name":"경기도","upr_cd":"6410000","map_id":"41"},{"name":"광주광역시","upr_cd":"6290000","map_id":"29"},{"name":"대구광역시","upr_cd":"6270000","map_id":"27"},{"name":"대전광역시","upr_cd":"6300000","map_id":"30"},{"name":"부산광역시","upr_cd":"6260000","map_id":"26"},{"name":"서울특별시","upr_cd":"6110000","map_id":"11"},{"name":"세종특별자치시","upr_cd":"5690000","map_id":"36"},{"name":"울산광역시","upr_cd":"6310000","map_id":"31"},{"name":"인천광역시","upr_cd":"6280000","map_id":"28"}];
+	// key : upr_cd
+	//var mapper = {"5690000":"36","6110000":"11","6260000":"26","6270000":"27","6280000":"28","6290000":"29","6300000":"30","6310000":"31","6410000":"41","6420000":"42"};
+	// key : map_id
+	//var mapper = {"11":"6110000","26":"6260000","27":"6270000","28":"6280000","29":"6290000","30":"6300000","31":"6310000","36":"5690000","41":"6410000","42":"6420000"};
+	// key : upr_cd,map_id
+	var mapper = {"11":{"upr_cd":"6110000","map_id":"11","name":"서울특별시","latitude":37.6,"longitude":127.002},"26":{"upr_cd":"6260000","map_id":"26","name":"부산광역시","latitude":35.25,"longitude":129},"27":{"upr_cd":"6270000","map_id":"27","name":"대구광역시","latitude":35.87222,"longitude":128.6025},"28":{"upr_cd":"6280000","map_id":"28","name":"인천광역시","latitude":37.55,"longitude":126.65},"29":{"upr_cd":"6290000","map_id":"29","name":"광주광역시","latitude":35.18,"longitude":126.85},"30":{"upr_cd":"6300000","map_id":"30","name":"대전광역시","latitude":36.3,"longitude":127.43},"31":{"upr_cd":"6310000","map_id":"31","name":"울산광역시","latitude":35.53889,"longitude":129.31},"36":{"upr_cd":"5690000","map_id":"36","name":"세종특별자치시","latitude":36.6,"longitude":127.28167},"41":{"upr_cd":"6410000","map_id":"41","name":"경기도","latitude":37.3,"longitude":127.3},"42":{"upr_cd":"6420000","map_id":"42","name":"강원도","latitude":37.7,"longitude":128.3},"5690000":{"upr_cd":"5690000","map_id":"36","name":"세종특별자치시","latitude":36.6,"longitude":127.28167},"6110000":{"upr_cd":"6110000","map_id":"11","name":"서울특별시","latitude":37.6,"longitude":127.002},"6260000":{"upr_cd":"6260000","map_id":"26","name":"부산광역시","latitude":35.25,"longitude":129},"6270000":{"upr_cd":"6270000","map_id":"27","name":"대구광역시","latitude":35.87222,"longitude":128.6025},"6280000":{"upr_cd":"6280000","map_id":"28","name":"인천광역시","latitude":37.55,"longitude":126.65},"6290000":{"upr_cd":"6290000","map_id":"29","name":"광주광역시","latitude":35.18,"longitude":126.85},"6300000":{"upr_cd":"6300000","map_id":"30","name":"대전광역시","latitude":36.3,"longitude":127.43},"6310000":{"upr_cd":"6310000","map_id":"31","name":"울산광역시","latitude":35.53889,"longitude":129.31},"6410000":{"upr_cd":"6410000","map_id":"41","name":"경기도","latitude":37.3,"longitude":127.3},"6420000":{"upr_cd":"6420000","map_id":"42","name":"강원도","latitude":37.7,"longitude":128.3}};
+	return mapper[cd||""]||"";
+}
+
+var upkindArr = [417000,422400,429900];
+var upkindObj = {417000:{cd:417000,name:"강아지",color:"rgb(82, 161, 243)"},422400:{cd:422400,name:"고앵이",color:"rgb(166, 123, 251)"},429900:{cd:429900,name:"기타",color:"rgb(144, 144, 144)"}};
+//축종코드
+function getUpkindNm(cd) {
+	if (cd == "417000") {
+		return "강아지";
+	} else if (cd == "422400") {
+		return "고양이";
+	} else if (cd == "429900") {
+		return "기타";
+	}
+	return "";
+}

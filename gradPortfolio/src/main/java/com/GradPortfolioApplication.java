f5a733c16e8ec0cdc2242023db83a1e1f43366fc
JMG <aaa4285@naver.com>
2019-11-14 01:28:19 +0900

    test

--- gradPortfolio/src/main/java/com/GradPortfolioApplication.java
+++ gradPortfolio/src/main/java/com/GradPortfolioApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@PropertySource("aws.properties")
 public class GradPortfolioApplication{
 	
 	public static void main(String[] args) {

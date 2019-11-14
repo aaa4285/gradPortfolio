3e7eb034fad0f9a13cd8e4c9ef3b28dd3a961e13
Nahyun <Nahyun@Nahyun-PC>
2019-11-14 23:42:44 +0900

    ts

--- gradPortfolio/src/main/java/com/GradPortfolioApplication.java
+++ gradPortfolio/src/main/java/com/GradPortfolioApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableScheduling
 @ComponentScan
 @PropertySource(value = {"aws.properties"}, ignoreResourceNotFound = true)
 public class GradPortfolioApplication{
